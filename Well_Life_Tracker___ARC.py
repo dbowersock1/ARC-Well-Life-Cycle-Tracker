d1f3f0eb80ee588a3330713ed6e76972ac75c94c
Dylan Bowersock <dbowersock18@gmail.com>
2021-10-21 08:23:35 -0600

    Well Object Expansion

--- Well_Life_Tracker___ARC.py
+++ Well_Life_Tracker___ARC.py
@@ -13,17 +13,36 @@ def main():
 	listOfWells = {}
 	# df['UWI'] is a dataframe series
 	# Note that the code below cycles through all the entry points, regardless of duplicate entries! Fix
+	# Would be nice to iterate through the entire sheet once!
 	for i in df['UWI']:
 		listOfWells[i] = Well(i)
 
 	for key in listOfWells:
 		print(listOfWells[key].UWI)
 
+	# adds job to individual UWI
+	print(listOfWells["102/02-12-066-25W5/00"])
+	print(df.loc[0])
+	listOfWells["102/02-12-066-25W5/00"].addJob(df.loc[0])
+	print(listOfWells["102/02-12-066-25W5/00"].numOfJobs)
+
 	print("End")
 
 class Well: 
-	def __init__(self,UWI):
+	def __init__(self, UWI):
 		self.UWI = UWI
+		self.numOfJobs=0
+		self.wrkArray = []
+
+	def addJob(self, dfSeries):
+		self.numOfJobs+=1
+		jobDict = { 
+			"jobCategory" : dfSeries["Job Category"],
+			"primaryJobType": dfSeries["Primary Job Type"] ,
+			"startDate" : dfSeries["Start Date"],
+			"endDate" : dfSeries["End Date"]
+			}
+		self.wrkArray.append(jobDict)
 
 if __name__ == "__main__":
 	main()
